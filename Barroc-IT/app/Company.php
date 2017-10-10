9615fdb2202c71a853fa4813eb15df2b8b9e254a
Unknown <d233407@edu.rocwb.nl>
2017-10-10 13:22:20 +0200

    Company model

--- Barroc-IT/app/Company.php
+++ Barroc-IT/app/Company.php
@@ -6,9 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Companies extends Model
 {
-<<<<<<< HEAD:Barroc-IT/app/Company.php
     protected $table = 'tbl_company';
-=======
-
->>>>>>> master:Barroc-IT/app/Companies.php
 }
